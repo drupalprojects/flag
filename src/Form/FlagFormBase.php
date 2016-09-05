eaa4fed462beaa64330246f6e7cc536f7f9bc70b
martin107 <martin107@2659379.no-reply.drupal.org>
2016-09-05 13:04:04 -0500

    Issue #2792163 by Pradnya Pingat, martin107: Removed Deprecated EntityInterface::urlInfo() from Flag code base.

--- src/Form/FlagFormBase.php
+++ src/Form/FlagFormBase.php
@@ -308,7 +308,7 @@ abstract class FlagFormBase extends EntityForm {
 
     $flag->enable();
     $status = $flag->save();
-    $url = $flag->urlInfo();
+    $url = $flag->toUrl();
     if ($status == SAVED_UPDATED) {
       drupal_set_message(t('Flag %label has been updated.', ['%label' => $flag->label()]));
       $this->logger('flag')->notice('Flag %label has been updated.', ['%label' => $flag->label(), 'link' => $this->l($this->t('Edit'), $url)]);
