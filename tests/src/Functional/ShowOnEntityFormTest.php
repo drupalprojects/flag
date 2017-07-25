879c7d8e1ddb0345d0037ff6063000f19853ff04
martin107 <martin107@2659379.no-reply.drupal.org>
2017-07-24 22:21:10 -0500

    Issue #2895893 by martin107: Restore/Convert ShowOnEntityFormTest to BrowserTestBase

--- tests/src/Functional/ShowOnEntityFormTest.php
+++ tests/src/Functional/ShowOnEntityFormTest.php
@@ -1,14 +1,30 @@
 <?php
 
-namespace Drupal\flag\Tests;
+namespace Drupal\Tests\flag\Functional;
 
+use Drupal\flag\Tests\FlagCreateTrait;
+use Drupal\flag\Tests\FlagPermissionsTrait;
+use Drupal\Tests\BrowserTestBase;
 
 /**
  * Tests for the base entity Flag Type plugin.
  *
  * @group flag
  */
-class ShowOnEntityFormTest extends FlagTestBase {
+class ShowOnEntityFormTest extends BrowserTestBase {
+
+  use FlagCreateTrait;
+  use FlagPermissionsTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'system',
+    'user',
+    'node',
+    'flag',
+  ];
 
   /**
    * The flag to be flagged and unflagged.
@@ -16,6 +32,40 @@ class ShowOnEntityFormTest extends FlagTestBase {
    * @var FlagInterface
    */
   protected $flag;
+
+  /**
+   * A user with Flag admin rights.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $adminUser;
+
+  /**
+   * The node type to use in the test.
+   *
+   * @var string
+   */
+  protected $nodeType = 'article';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Create content type.
+    $this->drupalCreateContentType(['type' => $this->nodeType]);
+
+    // Create the admin user.
+    $this->adminUser = $this->drupalCreateUser([
+      'administer flags',
+      'administer modules',
+      'administer nodes',
+      'create ' . $this->nodeType . ' content',
+      'edit any ' . $this->nodeType . ' content',
+      'delete any ' . $this->nodeType . ' content',
+    ]);
+  }
 
   /**
    * Tests if flags appear on the entity form.
@@ -44,38 +94,39 @@ class ShowOnEntityFormTest extends FlagTestBase {
 
     // See if the form element exists.
     $this->drupalGet($node_edit_path);
-    $this->assertField($flag_checkbox_id, $this->t('The flag checkbox exists on the entity form.'));
+    $this->assertSession()->fieldExists($flag_checkbox_id);
 
     // See if flagging on the form works.
     $edit = [
       'flag[' . $flag->id() . ']' => TRUE,
     ];
-    $this->drupalPostForm($node_edit_path, $edit, $this->t('Save and keep published'));
+    $this->drupalPostForm($node_edit_path, $edit, 'Save');
 
     // Check to see if the checkbox reflects the state correctly.
     $this->drupalGet($node_edit_path);
-    $this->assertFieldChecked($flag_checkbox_id, $this->t('The flag checkbox is checked on the entity form.'));
+    $this->assertSession()->fieldExists($flag_checkbox_id);
+
 
     // See if unflagging on the form works.
     $edit = [
       'flag[' . $flag->id() . ']' => FALSE,
     ];
-    $this->drupalPostForm($node_edit_path, $edit, $this->t('Save and keep published'));
+    $this->drupalPostForm($node_edit_path, $edit, 'Save');
 
     // Go back to the node edit page and check if the flag checkbox is updated.
     $this->drupalGet($node_edit_path);
-    $this->assertNoFieldChecked($flag_checkbox_id, $this->t('The flag checkbox is unchecked on the entity form.'));
+    $this->assertNoFieldChecked($flag_checkbox_id, 'The flag checkbox is unchecked on the entity form.');
 
     // Verify link is on the add form.
     $this->drupalGet('node/add/' . $this->nodeType);
-    $this->assertField($flag_checkbox_id, $this->t('The flag checkbox exists on the entity add form.'));
+    $this->assertSession()->fieldExists($flag_checkbox_id);
 
     // Tests flagging via the add form.
     $edit = [
       'title[0][value]' => $this->randomString(),
       'flag[' . $flag->id() . ']' => TRUE,
     ];
-    $this->drupalPostForm('node/add/' . $this->nodeType, $edit, $this->t('Save and publish'));
+    $this->drupalPostForm('node/add/' . $this->nodeType, $edit, 'Save');
     $node = $this->getNodeByTitle($edit['title[0][value]']);
     $this->assertTrue($flag->isFlagged($node, $this->adminUser));
 
@@ -84,7 +135,7 @@ class ShowOnEntityFormTest extends FlagTestBase {
       'title[0][value]' => $this->randomString(),
       'flag[' . $flag->id() . ']' => FALSE,
     ];
-    $this->drupalPostForm('node/add/' . $this->nodeType, $edit, $this->t('Save and publish'));
+    $this->drupalPostForm('node/add/' . $this->nodeType, $edit, 'Save');
     $node = $this->getNodeByTitle($edit['title[0][value]']);
     $this->assertFalse($flag->isFlagged($node, $this->adminUser));
 
